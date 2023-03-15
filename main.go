48610c72ee6f6b87495d6fa6ad1e566dbf1d62d7
cocryv <valentincocry@gmail.com>
2023-03-15 01:42:36 +0100

    implement cache

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -12,6 +13,12 @@ import (
 
 type ReverseProxy struct {
 	demoUrl *url.URL
+	cache   map[string]cacheItem
+}
+
+type cacheItem struct {
+	data       []byte
+	expiration time.Time
 }
 
 func (rp *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -27,22 +34,60 @@ func (rp *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		Str("remoteAddr", r.RemoteAddr).
 		Msg("Received request")
 
-	resp, err := http.DefaultClient.Do(r)
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to proxy request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	if _, ok := (rp.cache[rp.demoUrl.Host]); ok && time.Now().Before(rp.cache[rp.demoUrl.Host].expiration) {
+		log.Info().Msg("Cache hit")
 
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
+		w.WriteHeader(http.StatusOK)
+		// set Forwarded header
+		w.Header().Set("X-Forwarded-For", r.RemoteAddr)
+
+		w.Write(rp.cache[rp.demoUrl.Host].data)
+
+	} else {
+		resp, err := http.DefaultClient.Do(r)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to proxy request")
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+
+		if resp.StatusCode >= 400 {
+			log.Error().
+				Str("host", r.Host).
+				Str("method", r.Method).
+				Str("path", r.URL.Path).
+				Str("remoteAddr", r.RemoteAddr).
+				Int("statusCode", resp.StatusCode).
+				Msg("Upstream server returned an error")
+
+			w.WriteHeader(resp.StatusCode)
+			return
+		}
+
+		log.Info().Msg("Cache miss")
+
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to read response body")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		rp.cache[rp.demoUrl.Host] = cacheItem{
+			data:       respBody,
+			expiration: time.Now().Add(1 * time.Minute),
+		}
+
+		w.Header().Set("X-Forwarded-For", r.RemoteAddr)
+		w.WriteHeader(resp.StatusCode)
+		w.Write(respBody)
+	}
 
 	// Log the outgoing response
 	log.Info().
 		Str("host", r.Host).
 		Str("method", r.Method).
 		Str("path", r.URL.Path).
-		Int("statusCode", resp.StatusCode).
 		Msg("Completed request")
 }
 
@@ -57,7 +102,9 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to parse demo url")
 	}
 
-	rp := &ReverseProxy{demoUrl}
+	cache := make(map[string]cacheItem)
+
+	rp := &ReverseProxy{demoUrl, cache}
 
 	http.ListenAndServe(":8080", rp)
 }
